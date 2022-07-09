ea08f1b2a5b0e1f9ef32ccfd6ab852c7b50a87dd
Tasha Hankewych <thankewych@gmail.com>
2022-07-08 23:39:22 -0500

    Implement delete task functionality

--- src/App.js
+++ src/App.js
@@ -25,10 +25,18 @@ function App() {
   ]
   )
 
+  // Delete Task
+  const deleteTask = (id) => {
+    console.log('delete', id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ?
+        <Tasks tasks={tasks} onDeleteTask={deleteTask}/> :
+        'No tasks to show'}
     </div>
     );
 }
